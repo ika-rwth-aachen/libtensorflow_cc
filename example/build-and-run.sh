cd09f62c75f78d531198c054470bdc6b1d86212e
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2022-09-21 15:15:50 +0200

    switch example to cmake

--- example/build-and-run.sh
+++ example/build-and-run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-g++ -I /usr/local/include/tensorflow \
-    hello_tensorflow.cpp \
-    -o hello_tensorflow \
-    -Wl,-rpath,/usr/local/lib/tensorflow:/usr/local/lib \
-    /usr/local/lib/tensorflow/libtensorflow_cc.so \
-    /usr/local/lib/libprotobuf.so
-
-./hello_tensorflow
+apt update && apt install -y cmake
+cmake -S . -B build
+cmake --build build
+./build/hello_tensorflow
