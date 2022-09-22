5459b0241beb4c7767668f67e73c767cbca9f5f1
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2022-09-22 13:43:34 +0200

    switch example back to plain gcc
    cmake is not installed in images

--- example/build-and-run.sh
+++ example/build-and-run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-apt update && apt install -y cmake
-cmake -S . -B build
-cmake --build build
-./build/hello_tensorflow
+g++ -I /usr/local/include/tensorflow \
+    hello_tensorflow.cpp \
+    -o hello_tensorflow \
+    -Wl,-rpath,/usr/local/lib/tensorflow:/usr/local/lib \
+    /usr/local/lib/tensorflow/libtensorflow_cc.so \
+    /usr/local/lib/libprotobuf.so
+
+./hello_tensorflow
