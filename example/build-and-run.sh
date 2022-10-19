98db1bbbb6b6706bb9865ef9686405e041586f5c
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2022-10-19 16:55:52 +0200

    move lib out of dedicated folder in /usr/local/lib
    helps the linker to automatically find the library

--- example/build-and-run.sh
+++ example/build-and-run.sh
@@ -3,9 +3,7 @@ set -e
 
 g++ -I /usr/local/include/tensorflow \
     hello_tensorflow.cpp \
-    -o hello_tensorflow \
-    -Wl,-rpath,/usr/local/lib/tensorflow:/usr/local/lib \
-    /usr/local/lib/tensorflow/libtensorflow_cc.so \
-    /usr/local/lib/libprotobuf.so
+    -ltensorflow_cc -lprotobuf \
+    -o hello_tensorflow
 
 ./hello_tensorflow
